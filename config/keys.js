5016dc384c916f03ca35c17ce494fa4f44448616
kaweesa01 <kawesa.moses02@gmail.com>
2020-06-09 10:37:51 +0300

    finished db project

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-        MongoURL: 'mongodb://localhost:27017/memories'
+        MongoURL: 'mongodb+srv://kaweesa_1:20021507@cluster0-xmnvw.mongodb.net/memories?retryWrites=true&w=majority'
 }
